441fd83f21844197d6d6417044429e250c3d6190
Alpha Mureti <alphamureti@gmail.com>
2024-10-01 10:21:00 +0300

    initial push

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { startTransition } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import StarRating from "./starRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* {
+      <StarRating
+        maxRating={5}
+        messages={["Terrible", "Bad", "Ok", "Amaizing"]}
+        default={3}
+      />
+    } */}
   </React.StrictMode>
 );
 
